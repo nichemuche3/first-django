04919f9ffd663feb84b5d0e7e123000d19ffede1
Вадим Якименко <nichemuche3@gmail.com>
2025-10-21 11:18:42 +0300

    Import books

--- 2.1-databases/models_list_displaying/books/models.py
+++ 2.1-databases/models_list_displaying/books/models.py
@@ -2,11 +2,10 @@
 
 from django.db import models
 
-
 class Book(models.Model):
-    name = models.CharField(u'Название', max_length=64)
-    author = models.CharField(u'Автор', max_length=64)
-    pub_date = models.DateField(u'Дата публикации')
+    name = models.CharField(max_length=200)  
+    author = models.CharField(max_length=100)
+    pub_date = models.DateField()
 
     def __str__(self):
-        return self.name + " " + self.author
+        return f"{self.name} - {self.author}"
--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
-
+from .models import Book
 
 def books_view(request):
-    template = 'books/books_list.html'
-    context = {}
-    return render(request, template, context)
+    """Страница всех книг"""
+    books = Book.objects.all().order_by('pub_date', 'name')
+    return render(request, 'books/books_list.html', {'books': books})  
+
+def books_by_date_view(request, pub_date):
+    """Страница книг за конкретную дату"""
+    books = Book.objects.filter(pub_date=pub_date).order_by('name')
+    
+    
+    prev_book = Book.objects.filter(
+        pub_date__lt=pub_date
+    ).order_by('-pub_date').first()
+    
+    next_book = Book.objects.filter(
+        pub_date__gt=pub_date
+    ).order_by('pub_date').first()
+
+    context = {
+        'books': books,
+        'current_date': pub_date,
+        'prev_date': prev_book.pub_date if prev_book else None,
+        'next_date': next_book.pub_date if next_book else None,
+    }
+    
+    return render(request, 'books/books_list.html', context)  
