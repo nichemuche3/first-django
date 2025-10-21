cc50eb2dbcbf3adef2f6d3eb387d7582ca6285e1
Вадим Якименко <nichemuche3@gmail.com>
2025-10-21 10:10:49 +0300

    Import phone

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,32 @@
 import csv
-
 from django.core.management.base import BaseCommand
+from django.utils.dateparse import parse_date
 from phones.models import Phone
 
-
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
+    help = 'Import phones from CSV file'
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
-
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        # Файл phones.csv находится в корне проекта
+        csv_file = 'phones.csv'
+        
+        with open(csv_file, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file, delimiter=';')
+            
+            for row in reader:
+                phone_data = {
+                    'id': int(row['id']),
+                    'name': row['name'],
+                    'price': int(row['price']),
+                    'image': row['image'],
+                    'release_date': parse_date(row['release_date']),
+                    'lte_exists': row['lte_exists'].lower() == 'true'
+                }
+                 
+                phone = Phone(**phone_data)
+                phone.save()
+                self.stdout.write(f'Imported: {phone.name}')
+            
+            self.stdout.write(
+                self.style.SUCCESS(f'Successfully imported all phones')
+            )
